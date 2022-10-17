bff9c851d47354ffc367d580857299e979d5eb23
practica de git <jpromerog.usm@gmail.com>
2022-10-17 11:43:46 -0300

    jugando con el dom

---
+++ app.js
@@ -0,0 +1,4 @@
+const inputColor = document.querySelector('#inputColor')
+const btnVisualizar = document.querySelector('#btnVisualizar')
+const parrafoExa = document.querySelector('#parrafoExa')
+const cardColor = document.querySelector('#cardColor')
